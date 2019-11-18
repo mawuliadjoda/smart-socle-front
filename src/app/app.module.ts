10e57186f4c7a9cb729cfdf267b81d884638de03
Mawuli ADJODA <koffimawuli.adjoda@gmail.com>
2019-11-18 23:00:51 +0100

    add crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularMaterialModule } from './angular-material-module';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-import { NavComponent } from './nav/nav.component';
-import { ClientComponent } from './client/client.component';
-import { LoginComponent } from './login/login.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AngularMaterialModule } from "./angular-material-module";
+import { NavigationBarComponent } from "./navigation-bar/navigation-bar.component";
+import { NavComponent } from "./nav/nav.component";
+import { ClientComponent } from "./client/client.component";
+import { LoginComponent } from "./login/login.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { HomeComponent } from "./home/home.component";
+import { DeleteDialogComponent } from "./dialogs/delete/delete.dialog.component";
+import { EditDialogComponent } from "./dialogs/edit/edit.dialog.component";
+import { AddDialogComponent } from "./dialogs/add/add-dialog.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      NavigationBarComponent,
-      NavComponent,
-      ClientComponent,
-      LoginComponent,
-      PageNotFoundComponent,
-      HomeComponent
-   ],
-   imports: [
-      BrowserModule,
-      AppRoutingModule,
-      BrowserAnimationsModule,
-      AngularMaterialModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    NavigationBarComponent,
+    NavComponent,
+    ClientComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+    AddDialogComponent,
+    DeleteDialogComponent,
+    EditDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
+  ],
+  entryComponents: [
+    AddDialogComponent,
+    EditDialogComponent,
+    DeleteDialogComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
