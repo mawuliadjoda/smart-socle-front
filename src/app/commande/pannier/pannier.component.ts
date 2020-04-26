0c810d7a2eb142783a38fe1b2ca6e5344706bf15
Mawuli ADJODA <koffimawuli.adjoda@gmail.com>
2020-04-26 18:21:42 +0200

    fix display

--- src/app/commande/pannier/pannier.component.ts
+++ src/app/commande/pannier/pannier.component.ts
@@ -9,10 +9,11 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
 import { ProductState } from 'src/app/ngxs/state';
 import { Observable } from 'rxjs';
-import { Select } from '@ngxs/store';
+import { Select, Store } from '@ngxs/store';
 import * as fileSaver from 'file-saver'; // npm i --save file-saver
 import { LigneCommande } from 'src/app/models/ligne-commande';
 import { Router } from '@angular/router';
+import { UpdateProductToCart, DeleteProductToCart } from 'src/app/ngxs/action';
 @Component({
   selector: 'app-pannier',
   templateUrl: './pannier.component.html',
@@ -41,13 +42,16 @@ export class PannierComponent implements OnInit  {
   @Select(ProductState) state$: Observable<any>;
   cartTotal = 0;
 
+
+  oldQteValue: number;
   constructor(
     public dialog: MatDialog,
     public produitService: ProduitService,
     public fileService: FileService,
-    private _snackBar: MatSnackBar,
+    private snackBar: MatSnackBar,
     private sanitizer: DomSanitizer,
-    private router: Router
+    private router: Router,
+    private store: Store
   ) {  }
 
 
@@ -108,7 +112,9 @@ export class PannierComponent implements OnInit  {
       const key = o.id;
 
       const item = r.get(key) || Object.assign({}, o, {
-        qte: 0
+        qte: 0,
+        // permettre la modification de la qte du panier
+        isDisableUpdate: true
       });
 
       item.qte += o.qte;
@@ -118,12 +124,55 @@ export class PannierComponent implements OnInit  {
 
     console.log('===============================reduce:' + result);
     result.forEach(element => {
-      console.log(element.id, element.produit.nom, element.qte);
+      console.log(element.id, element.produit.nom, element.qte, element.isDisableUpdate);
     });
     return result;
   }
 
-  visualiserFacture(){
+  visualiserFacture() {
     this.router.navigateByUrl('smart/facture');
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////
+
+
+  startUpdateQte(ligneCommande: LigneCommande) {
+    ligneCommande.isDisableUpdate = false;
+    this.oldQteValue = ligneCommande.qte;
+  }
+
+  updateQte(ligneCommande) {
+    let message = '';
+    let action = '';
+
+    if (ligneCommande.produit.qte > ligneCommande.qte) {
+      this.store.dispatch(new UpdateProductToCart(ligneCommande.produit, ligneCommande.produit.id, ligneCommande.qte));
+      message = 'Qantité modifiée !';
+      action = 'Succès';
+    } else {
+      // si qte disponible du produit < qte de la commande alors on annule l'ajout au pannier et on remet la valeur inital du paanier
+      ligneCommande.qte = this.oldQteValue;
+      message = 'Quantité insuffisante !';
+      action = 'Erreur';
+    }
+    this.snackBar.open(message, action, {
+      duration: environment.durationOfSnackBar,
+    });
+
+    ligneCommande.isDisableUpdate = true;
+  }
+  removeSelection(selected: LigneCommande) {
+    const position = this.ligneCommandes.map(element =>  element.produit.id).indexOf(selected.produit.id);
+    // remove element in position
+    if (position !== -1) {
+      this.ligneCommandes.splice(position, 1);
+      this.store.dispatch(new DeleteProductToCart(selected.produit.id));
+    }
+
+    const action = 'sussès';
+    const message = `produit supprimé du pannier !`;
+    this.snackBar.open(message, action, {
+      duration: environment.durationOfSnackBar,
+    });
   }
 }
